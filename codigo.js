44efad91f0f6ab0979f1cc721512dc80caac5558
JairoSainz <68480276+JairoSainz@users.noreply.github.com>
2022-09-22 17:15:22 -0700

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,30 @@
+function showDiv2(){
+    document.getElementById('div5').style.display='none'
+    document.getElementById('div2').style.display='block';
+    document.getElementById('div1').style.display='none';
+    document.getElementById('div3').style.display='none';
+    document.getElementById('div4').style.display='none';
+
+
+}
+function showDiv3(){
+    document.getElementById('div5').style.display='none'
+    document.getElementById('div3').style.display='block';
+    document.getElementById('div1').style.display='none';
+    document.getElementById('div2').style.display='none';
+    document.getElementById('div4').style.display='none';
+}
+function showDiv4(){
+    document.getElementById('div5').style.display='none'
+    document.getElementById('div3').style.display='none';
+    document.getElementById('div1').style.display='none';
+    document.getElementById('div2').style.display='none';
+    document.getElementById('div4').style.display='block';
+}
+function showDiv5(){
+    document.getElementById('div5').style.display='block';
+    document.getElementById('div3').style.display='none';
+    document.getElementById('div1').style.display='none';
+    document.getElementById('div2').style.display='none';
+    document.getElementById('div4').style.display='none';
+}
